e9edd58a85f3766cecc918362ed4591debe99dcf
Adit-17 <yadavaditya17196@gmail.com>
2023-05-04 21:12:34 +0530

    updatedb2

---
+++ Divide1.java
@@ -0,0 +1,16 @@
+public class Divide1 {
+
+	
+	public static void main(String[] args) {
+     int a=10;
+     int b=20;
+     int c=a/b;
+    
+     System.out.println("the divide is ="  +  c);
+     
+      
+     
+
+	}
+
+}
