8d547d06d4e2f3a31351390cd5effd45dba65550
Adit-17 <yadavaditya17196@gmail.com>
2023-05-04 21:04:31 +0530

    updated

---
+++ Calculation.java
@@ -0,0 +1,16 @@
+public class Calculation {
+
+	
+	public static void main(String[] args) {
+     int a=10;
+     int b=20;
+     int c=a+b;
+     int d=a*b;
+     System.out.println("the sum is ="  +  c);
+      System.out.println("the PRODUCT is ="  +  d);
+      
+     
+
+	}
+
+}
